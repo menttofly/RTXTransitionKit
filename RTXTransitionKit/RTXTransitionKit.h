f12cf34e77d414a5dde999db1b57705ed4aeb22b
menttofly <1028365614@qq.com>
2018-09-17 23:56:13 +0800

    Add public header

---
+++ RTXTransitionKit/RTXTransitionKit.h
@@ -0,0 +1,21 @@
+//
+//  RTXTransitionKit.h
+//  RTXTransitionKit <https://github.com/menttofly/RTXTransitionKit>
+//
+//  Created by menttofly on 2018/9/17.
+//  Copyright © 2018年 menttofly. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#if __has_include(<RTXTransitionKit/RTXTransitionKit.h>)
+#import <RTXTransitionKit/UIViewController+RTXTransitionKit.h>
+#import <RTXTransitionKit/RTXNavigationController.h>
+#import <RTXTransitionKit/RTXViewController.h>
+#import <RTXTransitionKit/RTXNavigationBar.h>
+#else
+#import "RTXTransitionKit/UIViewController+RTXTransitionKit.h"
+#import "RTXTransitionKit/RTXNavigationController.h"
+#import "RTXTransitionKit/RTXViewController.h"
+#import "RTXTransitionKit/RTXNavigationBar.h"
+#endif
